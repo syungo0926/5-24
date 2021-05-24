10b40c63328aed78950df8bbfb244ac78f28f31a
business09 <business09@2020business09noMacBook-Air.local>
2021-05-24 16:39:21 +0900

    5-24

---
+++ html/index.php
@@ -0,0 +1,60 @@
+<?php
+    $kekka = 0;
+
+    sleft = 0
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <title>松野駿吾</title>
+  </head>
+  <body>
+    <h1>松野駿吾</h1>
+    <form class="row row-cols-lg-auto g-3 align-items-center">
+  <div class="col-12">
+    <div class="input-group">
+      <input type="text" class="form-control" id="inlineFormInputGroupUsername" >
+    </div>
+  </div>
+  <div class="col-12">
+    <label class="visually-hidden" for="inlineFormSelectPref">Preference</label>
+    <select class="form-select" id="inlineFormSelectPref">
+    <option value="+">加算（＋）</option>
+    <option value="-">減算（ー）</option>
+    <option value="*">乗算（Ｘ）</option>
+    <option value="/">除算（÷）</option>
+    </select>
+  </div>
+
+  <div class="col-12">
+    <label class="visually-hidden" for="inlineFormInputGroupUsername">Username</label>
+    <div class="input-group">
+      <input type="text" class="form-control" id="inlineFormInputGroupUsername" >
+    </div>
+  </div>
+
+  <div class="col-12">
+    <button type="submit" class="btn btn-primary">=</button>
+  </div>
+  <div class="col-12">
+    <button type="submit" class="btn btn-primary">=</button>
+  </div>
+</form>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
